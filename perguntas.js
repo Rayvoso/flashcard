1ed8d421d1685c18a638387e73de4e8070c6299f
Rayvoso <00001106921963SP@al.educacao.sp.gov.br>
2024-12-03 09:38:25 -0300

    alteirando o js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Matemática',
+    'Qual é a raiz quadrada de 396?',
+    '13'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do brasil?',
+    'brasilia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'charadas',
+    'Quais são os três números, nenhum dos quais é zero, que dão o mesmo resultado, quer sejam somados ou multiplicados?',
+    '1, 2 e 3, porque:',
+    '1 + 2 + 3 = 6 e 1 x 2 x 3 = 6'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'charadas',
+    'Eu sou seu irmão, mas você não é meu irmão. Como será que isso é possível?',
+    'Você não é meu irmÃO, porque é minha irmÃ'
 )
